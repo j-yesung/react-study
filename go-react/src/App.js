bc234af61b8056bf3e47c6a07f166c35ccd01fdb
j-yesung <hisung-ah@kakao.com>
2023-10-31 16:22:35 +0900

    props 연습2

--- go-react/src/App.js
+++ go-react/src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-
-// props를 통해 부모 데이터를 전달받았다.
-function Son(props) {
-  console.log('props: ', props);
-  return <div>나는 {props.motherName}의 아들이에요!</div>;
-}
-
-// 부모 -> 자식 정보를 전달했다.
-function Mother() {
-  const name = '흥부인';
-  return <Son motherName={name} />;
-}
-
-function GrandFather() {
-  return <Mother />;
-}
-
 export default function App() {
-  return <GrandFather />;
+  return (
+    <div className="App">
+      <FirstComponent content="Who needs me?" />
+    </div>
+  );
+}
+
+function FirstComponent({ content }) {
+  return (
+    <div>
+      <h3>I am the first component</h3>
+      <SecondComponent content={content} />
+    </div>
+  );
+}
+
+function SecondComponent({ content }) {
+  return (
+    <div>
+      <h3>I am the second component</h3>
+      <ThirdComponent content={content} />
+    </div>
+  );
+}
+
+function ThirdComponent({ content }) {
+  return (
+    <div>
+      <h3>I am the third component</h3>
+      <ComponentNeedingProps content={content} />
+    </div>
+  );
+}
+
+function ComponentNeedingProps({ content }) {
+  return <h3>{content}</h3>;
 }
