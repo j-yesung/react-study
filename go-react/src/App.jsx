1620a57e355cbeac23c780924f8f006510ff723e
j-yesung <hisung-ah@kakao.com>
2023-11-07 11:22:30 +0900

    useEffect 연습

--- go-react/src/App.jsx
+++ go-react/src/App.jsx
@@ -1,45 +1,22 @@
 import './App.css';
-import styled from 'styled-components';
-import TestPage from './components/TestPage';
-import GlobalStyle from './components/GlobalStyle';
-
-// const StContainer = styled.div`
-//   display: flex;
-// `;
-// const StBox = styled.div`
-//   width: 100px;
-//   height: 100px;
-//   border: 1px solid ${props => props.borderColor};
-//   margin: 20px;
-// `;
-
-// 박스의 색
-// const boxList = ['red', 'blue', 'green'];
-
-// 색을 넣으면 이름 반환
-// const getBoxName = color => {
-//   switch (color) {
-//     case 'red':
-//       return '빨간 박스';
-//     case 'blue':
-//       return '파란 박스';
-//     case 'green':
-//       return '초록 박스';
-//     default:
-//       break;
-//   }
-// };
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  // clean up
+  useEffect(() => {
+    console.log(`hello useEffect! ${value}`);
+
+    // 컴포넌트가 죽을 때, 실행
+    return () => {
+      console.log('사라짐!');
+    };
+  }, [value]);
   return (
-    // <StContainer>
-    //   {boxList.map(box => {
-    //     return <StBox borderColor={box}>{getBoxName(box)}</StBox>;
-    //   })}
-    // </StContainer>
     <>
-      <GlobalStyle />
-      <TestPage title="제목입니다." contents="내용입니다."></TestPage>
+      <div>
+        <input type="text" value={value} onChange={event => setValue(event.target.value)} />
+      </div>
     </>
   );
 }
