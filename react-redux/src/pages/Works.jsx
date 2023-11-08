ea7cd29698ceb1f618c1bc3114fc81e4cc0eb124
j-yesung <hisung-ah@kakao.com>
2023-11-08 14:55:01 +0900

    react-dom Link 적용

--- react-redux/src/pages/Works.jsx
+++ react-redux/src/pages/Works.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Works() {
-  return <div>Works</div>;
+  return (
+    <>
+      <div>Works</div>
+      <button>Home으로 이동</button>
+      <Link to="/contact">go contact</Link>
+    </>
+  );
 }
 
 export default Works;
