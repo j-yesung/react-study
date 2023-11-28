5c989e4e1e6fe406d53a4bc862b770acf612d8be
j-yesung <hisung-ah@kakao.com>
2023-11-28 11:19:52 +0900

    add redux toolkit & setting

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import Router from './shared/Router';
+import React, { useState } from 'react';
+// import Router from './shared/Router';
 
 function App() {
-  return <Router />;
+  // return <Router />;
+  const [count, setCount] = useState(0);
+
+  const addNumber = () => {
+    setCount(count + 1);
+  };
+  const minusNumber = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={addNumber}>+</button>
+      <button onClick={minusNumber}>-</button>
+    </>
+  );
 }
 
 export default App;
--- react-redux/src/redux/modules/counter.js
+++ react-redux/src/redux/modules/counter.js
@@ -1,20 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 // Action Creator에 사용할 value
-const PLUS_N = 'conuter/PLUS_N';
-const MINUS_N = 'conuter/MILUS_N';
+const ADD_NUMBER = 'counter/ADD_NUMBER';
+const MINUS_NUMBER = 'counter/MINUS_NUMBER';
 
 // Action Creator
-export const plusN = payload => {
-  return {
-    type: PLUS_N,
-    payload,
-  };
-};
-export const minusN = payload => {
-  return {
-    type: MINUS_N,
-    payload,
-  };
-};
+// export const plusN = payload => ({
+//   type: PLUS_N,
+//   payload,
+// });
+
+// export const minusN = payload => ({
+//   type: MINUS_N,
+//   payload,
+// });
 
 // 초기 상태값
 const initialState = {
@@ -23,15 +22,13 @@ const initialState = {
 
 // reducer
 const counter = (state = initialState, action) => {
-  console.log('state: ', state);
-  console.log('action: ', action);
   switch (action.type) {
-    case PLUS_N:
+    case ADD_NUMBER:
       return {
         ...state,
         number: state.number + action.payload,
       };
-    case MINUS_N:
+    case MINUS_NUMBER:
       return {
         ...state,
         number: state.number - action.payload,
@@ -41,4 +38,18 @@ const counter = (state = initialState, action) => {
   }
 };
 
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    ADD_NUMBER: (state, action) => {
+      state.number = state.number + action.payload;
+    },
+    MINUS_NUMBER: (state, action) => {
+      state.number = state.number - action.payload;
+    },
+  },
+});
+
 export default counter;
+// export {PLUS_N, MINUS_N} counterSlice.actions
