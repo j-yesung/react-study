fe6d1a133b554620bf3faebec75629855c01adb9
j-yesung <hisung-ah@kakao.com>
2023-11-28 17:33:26 +0900

    thunk를 이용한 비동기 처리 & 로딩, 에러 예외 처리

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import { useState } from 'react';
+// src/App.jsx
+
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { minusNumber, __addNumber } from './redux/modules/counter';
+import { __getTodos } from './redux/modules/todosSlice';
 
 const App = () => {
   const dispatch = useDispatch();
-  const [number, setNumber] = useState(0);
-  const globalNumber = useSelector(state => state.counter.number);
+  const { isLoading, error, todos } = useSelector(state => state.todos);
 
-  const onChangeHandler = evnet => {
-    const { value } = evnet.target;
-    setNumber(+value);
-  };
+  useEffect(() => {
+    dispatch(__getTodos());
+  }, [dispatch]);
 
-  // thunk 함수를 디스패치한다. payload는 thunk함수에 넣어주면,
-  // 리덕스 모듈에서 payload로 받을 수 있다.
-  const onClickAddNumberHandler = () => {
-    dispatch(__addNumber(number));
-  };
+  if (isLoading) {
+    return <div>로딩 중....</div>;
+  }
 
-  const onClickMinusNumberHandler = () => {
-    dispatch(minusNumber(number));
-  };
+  if (error) {
+    return <div>{error.message}</div>;
+  }
 
   return (
     <div>
-      <div>{globalNumber}</div>
-      <input type="number" onChange={onChangeHandler} />
-      <button onClick={onClickAddNumberHandler}>더하기</button>
-      <button onClick={onClickMinusNumberHandler}>빼기</button>
+      {todos.map(todo => (
+        <div key={todo.id}>{todo.title}</div>
+      ))}
     </div>
   );
 };
